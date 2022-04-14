74f2e20eb31db9a45046e80e09749eeba519db02
HannaSubil <hannasubil24@gmail.com>
2022-04-14 22:39:05 +0530

    commit

--- app/src/main/java/com/hanna/myfirstapplication/RegistrationPage.java
+++ app/src/main/java/com/hanna/myfirstapplication/RegistrationPage.java
@@ -1,13 +1,54 @@
 package com.hanna.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class RegistrationPage extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegistrationPage extends AppCompatActivity implements View.OnClickListener {
+
+    Button register;
+    EditText name,age,place,uname,pword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        register=(Button) findViewById(R.id.submit_button);
+        name=(EditText)findViewById(R.id.nametext) ;
+        age=(EditText)findViewById(R.id.agetext) ;
+        place=(EditText)findViewById(R.id.placetext) ;
+        uname=(EditText)findViewById(R.id.usernametext) ;
+        pword=(EditText)findViewById(R.id.passwodtext) ;
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (name.getText().toString().matches("") || age.getText().toString().matches("") || place.getText().toString().matches("") ||uname.getText().toString().matches("") || pword.getText().toString().matches(""))
+        {
+            Toast.makeText(getApplicationContext(),"Please Fill All Fields",Toast.LENGTH_LONG).show();
+
+
+
+        }
+        else{
+            String un=uname.getText().toString();
+            String pw=pword.getText().toString();
+            Intent i = new Intent(getApplicationContext(),loginpage.class);
+            i.putExtra("user",un);
+            i.putExtra("password",pw);
+            startActivity(i);
+
+        }
+
+
     }
 }
--- app/src/main/java/com/hanna/myfirstapplication/loginpage.java
+++ app/src/main/java/com/hanna/myfirstapplication/loginpage.java
@@ -12,57 +12,78 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class loginpage extends AppCompatActivity implements View.OnClickListener {
+public class loginpage extends AppCompatActivity implements View.OnClickListener
+{
 
+    Intent registerintent;
     TextView loginlabel;
     Button login;
     EditText un,pw;
+    TextView register_me_label;
 
-    String username="admin";
-    String password="12345";
+    String username ;
+    String password;
+
     int count=3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
+        register_me_label=(TextView)findViewById(R.id.registration);
 
         login = (Button) findViewById(R.id.login);
         un=(EditText)findViewById(R.id.username);
         pw=(EditText)findViewById(R.id.password);
+
         loginlabel=(TextView)findViewById(R.id.loginpage);
 
-    login.setOnClickListener((this));
-           }
-    @Override
-    public void onClick(View view) {
-
-        String uname=un.getText().toString();
-        String pword=pw.getText().toString();
-
-        if (username.equals(uname) && password.equals(pword)) {
-
-            Intent i = new Intent(getApplicationContext(),HomePage.class);
-            //passing username to Homepage through intent
-            i.putExtra("user",uname);
-            startActivity(i);
-
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"Invalid Username or Password",Toast.LENGTH_LONG).show();
-            count--;
 
 
-                if(count == 0){
+    login.setOnClickListener(this);
+    register_me_label.setOnClickListener(this);
 
-                    login.setEnabled(false); //disabling the Button
-                    loginlabel.setText("LOGIN BLOCKED");
-                    loginlabel.setTextColor(Color.RED);
+        registerintent=getIntent();
+         username = registerintent.getStringExtra("user");
+         password=registerintent.getStringExtra("password");
 
-            }
-        }
+        un.setText(username);
+        un.setEnabled(true);
 
-
-      }
 
     }
+    @Override
+    public void onClick(View view)
+    {
+
+
+        String uname = un.getText().toString();
+               String pword = pw.getText().toString();
+        if(view.getId()==R.id.login) {
+               if (username.equals(uname) && password.equals(pword)) {
+                   Intent i = new Intent(getApplicationContext(), HomePage.class);
+                   //passing username to Homepage through intent
+                   i.putExtra("user", uname);
+                   startActivity(i);
+
+               } else {
+                   Toast.makeText(getApplicationContext(), "Invalid Username or Password", Toast.LENGTH_LONG).show();
+                   count--;
+                   if (count == 0) {
+                       login.setEnabled(false); //disabling the Button
+                       loginlabel.setText("LOGIN BLOCKED");
+                       loginlabel.setTextColor(Color.RED);
+
+                   }
+               }
+           }
+                if (view.getId()==R.id.registration)
+                {
+
+                    Intent i = new Intent(getApplicationContext(),RegistrationPage.class);
+                    startActivity(i);
+
+        }
+    }
+}
